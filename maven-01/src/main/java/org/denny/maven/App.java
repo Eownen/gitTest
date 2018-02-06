99407745432191afae63a3498976d05138f915fd
eownen <denny_loong@outlook.com>
2018-02-06 13:13:40 +0800

    测试删除！

---
+++ maven-01/src/main/java/org/denny/maven/App.java
@@ -0,0 +1,15 @@
+package org.denny.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for ( String s : args ) {
+        	System.err.print(s + "、");
+        }
+    }
+}
---
+++ maven-01/src/test/java/org/denny/maven/AppTest.java
@@ -0,0 +1,15 @@
+package org.denny.maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase{
+	
+    public void testOrder() {
+    	System.err.println("Hello test!");
+    }
+}
